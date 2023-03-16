00f5878349d73f782d1f14658741e768a4490a91
ow1ngs <jd.sg1@hotmail.com>
2023-03-15 20:25:00 -0500

    feat: implement firestore configuration

--- firebaseconfig.js
+++ firebaseconfig.js
@@ -1,17 +1,19 @@
-import { initializeApp } from "firebase/app";
-import { getAuth } from 'firebase/auth';
+import { initializeApp } from 'firebase/app'
+import { getAuth } from 'firebase/auth'
+import { getFirestore } from 'firebase/firestore'
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDMM2XZPekbfZxfyquDom836s7W4IYmI_Y",
-  authDomain: "juanzapp-8ede7.firebaseapp.com",
-  projectId: "juanzapp-8ede7",
-  storageBucket: "juanzapp-8ede7.appspot.com",
-  messagingSenderId: "785746597508",
-  appId: "1:785746597508:web:1d27e54b9dfc56373a54d1",
-  measurementId: "G-3HC2L392NX"
-};
+  apiKey: 'AIzaSyDMM2XZPekbfZxfyquDom836s7W4IYmI_Y',
+  authDomain: 'juanzapp-8ede7.firebaseapp.com',
+  projectId: 'juanzapp-8ede7',
+  storageBucket: 'juanzapp-8ede7.appspot.com',
+  messagingSenderId: '785746597508',
+  appId: '1:785746597508:web:1d27e54b9dfc56373a54d1',
+  measurementId: 'G-3HC2L392NX',
+}
 
-const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
+const app = initializeApp(firebaseConfig)
+const auth = getAuth()
+const db = getFirestore()
 
-export { auth }
+export { auth, db, app }
